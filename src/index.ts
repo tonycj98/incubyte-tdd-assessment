62f1d96deab6acd9df7ddca9a11c1191da6a3393
Tony James <64031592+tonycj98@users.noreply.github.com>
2026-03-17 16:43:47 +0000

    ✨ feat: implement handle new lines between numbers (instead of commas)
     ✓ index.test.ts (6 tests) 4ms
       ✓ incubyte-tdd-assessment: Kata 1: String Calculator (6)
         ✓ test 01: should export and import the function without errors 2ms
         ✓ test 02: should return zero for an empty string 0ms
         ✓ test 03: should return the number itself for a single number string 0ms
         ✓ test 04: should return the sum of two numbers in a string 0ms
         ✓ test 05: should return the sum of multiple numbers in a string 0ms
         ✓ test 06: should handle new lines between numbers (instead of commas) 0ms

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+const delimiterPattern = /[,|\n]+/
 export function add(numbers: string): number {
   if (numbers === '') {
     return 0
   }
 
-  return numbers.split(',').reduce((sum, num) => sum + Number.parseInt(num, 10), 0)
+  return numbers.split(delimiterPattern).reduce((sum, num) => sum + Number.parseInt(num, 10), 0)
 }
