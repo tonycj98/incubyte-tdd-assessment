bf8ec62970563c09f16d11a13133cfa99d58ffc5
Tony James <64031592+tonycj98@users.noreply.github.com>
2026-03-17 16:28:17 +0000

    ✨ feat: implement return the sum of two numbers in a string
     ✓ index.test.ts (4 tests) 11ms
       ✓ incubyte-tdd-assessment: Kata 1: String Calculator (4)
         ✓ test 01: should export and import the function without errors 4ms
         ✓ test 02: should return zero for an empty string 1ms
         ✓ test 03: should return the number itself for a single number string 3ms
         ✓ test 04: should return the sum of two numbers in a string 1ms

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,10 @@ export function add(numbers: string): number {
   if (numbers === '') {
     return 0
   }
-  return Number.parseInt(numbers, 10)
+
+  const numsArray = numbers.split(',')
+
+  const numSum = Number.parseInt(numsArray[0] ?? '0', 10) + Number.parseInt(numsArray[1] ?? '0', 10)
+
+  return numSum
 }
